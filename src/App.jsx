fcb07f8ee28215b3b3c98e9311a516d0d5875d73
dbd01350-eng <dbd01350@gmail.com>
2026-07-14 10:55:59 +0900

    Add initial project setup with React and Vite
    - Created main entry point for React application in `main.jsx`
    - Added Vite configuration file for project setup
    - Included React and Babel plugins in Vite configuration
    - Added SVG assets for React and Vite logos

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+import { Text } from "./Text";
+import { Img } from "./Img";
+
+export default function App() {
+  return (
+    <>
+      <h1>메인 App</h1>
+      <Text></Text>
+      <Img />
+      <Button />
+    </>
+  );
+}
