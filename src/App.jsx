eb45d3f525cb2bcac39b78b991e6b82ec5ad33e3
dbd01350-eng <dbd01350@gmail.com>
2026-07-14 14:28:36 +0900

    Refactor App component and add new Child, Member, Picture components; remove unused Button, Img, and Text components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import Button from "./Button";
-import { Text } from "./Text";
-import { Img } from "./Img";
+import Child from "./Child";
+import Member from "./Memper.jsx";
+import Picture from "./Picture.jsx";
+import Style from "./Style.css";
 
-export default function App() {
+function Name() {
+  return <h1>App 안에서 태어난 컴포넌트</h1>;
+}
+
+function App() {
+  // 컴포넌트의 조건: 함수명이 대문자로 시작, return 값 있어야 하고 태그를 꼭 넣어야함.
+
+  /*   function Name() {
+    return <h1>App 안에서 태어난 컴포넌트</h1>
+  }  -> 여기서 추가 못함
+ */
+
   return (
-    <>
-      <h1>메인 App</h1>
-      <Text></Text>
-      <Img />
-      <Button />
-    </>
+    <div>
+      <Name></Name>
+      <h1>안녕하세요</h1>
+      <Child></Child>
+      <Child />
+      <Member></Member>
+      <Picture></Picture>
+    </div>
   );
 }
+export default App;
---
+++ src/Memper.jsx
@@ -0,0 +1,9 @@
+const Member = (props) => {
+  return (
+    <>
+      <div>이름:</div>
+      <div>김망고</div>
+    </>
+  );
+};
+export default Member;
